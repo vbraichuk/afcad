b602e0a36ad6f68f9cf925a0f96d46b6d4b39d6c
Vitalii Braichuk <vbraichuk@gmail.com>
2020-01-10 11:36:42 +0100

    u1

--- json2aurora.py
+++ json2aurora.py
@@ -9,17 +9,37 @@ def dec2dms(dec):
 	E_d = int(dec[0])
 	E_m = int((dec[0] - E_d) * 60)
 	E_s = (dec[0] - E_d - E_m / float(60)) * 3600
-	print "N{0:03d}.{1:02d}.{2:07.3f};E{3:03d}.{4:02d}.{5:07.3f};".format(N_d, N_m, N_s, E_d, E_m, E_s)
+	return "N{0:03d}.{1:02d}.{2:07.3f};E{3:03d}.{4:02d}.{5:07.3f};".format(N_d, N_m, N_s, E_d, E_m, E_s)
 
 def main():
 	with open("ukbb/ad_grass.geojson", "r") as read_file:
 		data = json.load(read_file)
 
 	for feature in data['features']:
-		print "// {} - {}".format(feature['properties']['desc'], feature['geometry']['type'])
-		for coordinates in feature['geometry']['coordinates']:
-			for point in coordinates:
-				dec2dms(point)
+		# Check if feature has key 'desc'
+		if feature['properties'].has_key('desc'):
+			desc = feature['properties']['desc']
+		else:
+			desc = "None"
+		
+		# Print comment line with Name and Type
+		print "// {} - {}".format(desc, feature['geometry']['type'])
+
+		# Print coordinates for Polygon without last point
+		if feature['geometry']['type'] == "Polygon":
+			for coordinates in feature['geometry']['coordinates']:
+				for point in coordinates[:-1]:
+					print dec2dms(point)
+		# Print coordinates for LineString with doubling second point
+		else:
+			i = 1
+			while i < len(feature['geometry']['coordinates']):
+				print "{}{}".format(dec2dms(feature['geometry']['coordinates'][i - 1]),
+									dec2dms(feature['geometry']['coordinates'][i]))
+				i += 1
+
+
+
 
 if __name__== "__main__":
 	main()
