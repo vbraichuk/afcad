cb853723104ce86825f5e54fc62f3dab706363fd
Vitalii Braichuk <vbraichuk@gmail.com>
2020-01-15 22:54:18 +0100

    Update json2aurora.py

--- json2aurora.py
+++ json2aurora.py
@@ -12,7 +12,7 @@ def dec2dms(dec):
 	return "N{0:03d}.{1:02d}.{2:06.3f};E{3:03d}.{4:02d}.{5:06.3f};".format(N_d, N_m, N_s, E_d, E_m, E_s)
 
 def main():
-	with open("country/dniepro.geojson", "r") as read_file:
+	with open("ukkk/everything.geojson", "r") as read_file:
 		data = json.load(read_file)
 
 	for feature in data['features']:
